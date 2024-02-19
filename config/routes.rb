8eea1a60e1075ff012472bed083d301ba1ac5211
Zeeshan Gulzar <xeeshangulzar@gmail.com>
2024-02-19 18:01:38 +0500

    Chat-app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'rooms/index'
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+  resources :rooms
+  resources :users
+  resources :rooms do
+    resources :messages
+  end
+  root 'rooms#index'
 end
